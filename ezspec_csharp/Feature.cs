b753011bbfb8fd7baa2be4aa43b681ef2bfa7362
CalvinWan0101 <calvinwan0101@gmail.com>
2023-11-14 21:06:38 +0800

    [Feature Addition] Added Feature static factory

---
+++ ezspec_csharp/Feature.cs
@@ -0,0 +1,31 @@
+﻿namespace ezspec_csharp_test {
+    
+    public class Feature {
+
+        private string name;
+        private string description;
+
+        public string Name {
+            get { return name; }
+        }
+
+        public string Description { 
+            get { return description; }
+        }
+
+        private Feature(string name, string description) {
+            this.name = name;
+            this.description = description;
+        }
+
+        static public Feature New(string name) {
+            return new Feature(name, "");
+        }
+
+        static public Feature New(string name, string description) {
+            return new Feature(name, description);
+        }
+
+    }
+
+}
---
+++ ezspec_csharp_test/FeatureTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ezspec_csharp_test {
+
+    [TestClass]
+    public class FeatureTest {
+
+        [TestMethod]
+        public void create_feature_with_name_and_description() {
+            Feature feature = Feature.New("name", "description");
+
+            Assert.AreEqual("name", feature.Name);
+            Assert.AreEqual("description", feature.Description);
+        }
+
+        [TestMethod]
+        public void create_feature_with_name() {
+            Feature feature = Feature.New("name");
+
+            Assert.AreEqual("name", feature.Name);
+            Assert.AreEqual("", feature.Description);
+        }
+
+    }
+
+}
