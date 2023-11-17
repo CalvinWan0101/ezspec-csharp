27d1fc68c48c401f0b5aa689ade6628e82e3a574
CalvinWan0101 <calvinwan0101@gmail.com>
2023-11-17 21:27:27 +0800

    [Feature Addition] Completed static factory and ToString for Rule

---
+++ ezspec_csharp/Rule.cs
@@ -0,0 +1,39 @@
+﻿namespace ezspec_csharp {
+
+    public class Rule {
+
+        private string name;
+        private string description;
+
+        public string Name {
+            get { return name; }
+        }
+
+        public string Description {
+            get { return description; }
+        }
+
+        private Rule(string name, string description) {
+            this.name = name;
+            this.description = description;
+        }
+
+        static public Rule New(string name) {
+            return new Rule(name, "");
+        }
+
+        static public Rule New(string name, string description) {
+            return new Rule(name, description);
+        }
+
+        public override string ToString() {
+            string result = "Rule: " + name;
+            if ("" != description) {
+                result += "\n" + description;
+            }
+            return result;
+        }
+
+    }
+
+}
---
+++ ezspec_csharp_test/RuleTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ezspec_csharp;
+
+namespace ezspec_csharp_test {
+
+    [TestClass]
+    public class RuleTest {
+
+        [TestMethod]
+        public void create_rule_with_name() {
+            Rule rule = Rule.New("name");
+
+            Assert.AreEqual("name", rule.Name);
+            Assert.AreEqual("", rule.Description);
+        }
+
+        [TestMethod]
+        public void create_rule_with_name_and_description() {
+            Rule rule = Rule.New("name", "description");
+
+            Assert.AreEqual("name", rule.Name);
+            Assert.AreEqual("description", rule.Description);
+        }
+
+        [TestMethod]
+        public void get_rule_string() {
+            Rule rule = Rule.New("name", "description");
+
+            Assert.AreEqual("Rule: name\ndescription", rule.ToString());
+        }
+
+        [TestMethod]
+        public void get_rule_string_without_description() {
+            Rule rule = Rule.New("name");
+
+            Assert.AreEqual("Rule: name", rule.ToString());
+        }
+
+    }
+
+}
