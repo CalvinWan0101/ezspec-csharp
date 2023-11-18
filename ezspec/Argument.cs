d6533bd35ff5967c8885297cd8a4b0727e1c1a51
CalvinWan0101 <calvinwan0101@gmail.com>
2023-11-18 22:00:26 +0800

    [Feature Addition] Added Argument static factory

---
+++ ezspec/Argument.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ezSpec {
+
+    public class Argument {
+
+        private string key;
+        private string value;
+
+        public string Key {
+            get { return key; }
+        }
+
+        public string Value {
+            get { return value; }
+        }
+
+        static public Argument Create(string expression) {
+            return new Argument(expression);
+        }
+
+        public Argument(string expression) {
+            if (IsKeyValuePair(expression)) {
+                InitializeWithKeyValuePair(expression);
+            } else {
+                InitializeWithValue(expression);
+            }
+        }
+
+        private bool IsKeyValuePair(string expression) {
+            return expression.Trim().StartsWith("${") && expression.Trim().EndsWith("}");
+        }
+
+        private void InitializeWithKeyValuePair(string expression) {
+            Regex reg = new Regex("^([^=:]+)[=:](.+)$");
+            MatchCollection matchs = reg.Matches(expression);
+            GroupCollection groups = matchs[0].Groups;
+
+            key = groups[1].Value.Trim().Substring(2).Trim();
+            value = groups[2].Value.Trim().Substring(0, groups[2].Value.Trim().Length - 1).Trim();
+        }
+
+        private void InitializeWithValue(string expression) {
+            Regex reg = new Regex("\\$(\\S)+");
+            MatchCollection matchs = reg.Matches(expression);
+            GroupCollection groups = matchs[0].Groups;
+
+            key = "";
+            value = groups[0].Value.Trim().Substring(1);
+        }
+
+    }
+
+}
---
+++ ezspec_test/ArgumentTest.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ezSpec.Test {
+
+    [TestClass]
+    public class ArgumentTest {
+
+        [TestMethod]
+        public void value_argument_start_with_dollar_sign() {
+            Assert.AreEqual("1", Argument.Create("$1").Value);
+            Assert.AreEqual("", Argument.Create("$1").Key);
+            Assert.AreEqual("80", Argument.Create("$80").Value);
+            Assert.AreEqual("\\$5", Argument.Create("$\\$5").Value);
+            Assert.AreEqual("$5", Argument.Create("$$5").Value);
+            Assert.AreEqual("$$5", Argument.Create("$$$5").Value);
+            Assert.AreEqual("$$20$20", Argument.Create("$$$20$20").Value);
+            Assert.AreEqual("vat:100", Argument.Create("$vat:100").Value);
+            Assert.AreEqual("vat=100", Argument.Create("$vat=100").Value);
+        }
+
+        [TestMethod]
+        public void key_value_argument_place_in_curly_brackets_with_equal_sign() {
+            Assert.AreEqual("price", Argument.Create("${price=21,000}").Key);
+            Assert.AreEqual("21,000", Argument.Create("${price=21,000}").Value);
+            Assert.AreEqual("price", Argument.Create("${price=  21,000     }").Key);
+            Assert.AreEqual("21,000", Argument.Create("${price=  21,000     }").Value);
+            Assert.AreEqual("price", Argument.Create("${    price =21,000}").Key);
+            Assert.AreEqual("21,000", Argument.Create("${    price =21,000}").Value);
+            Assert.AreEqual("price", Argument.Create("${ price   =   21,000 }").Key);
+            Assert.AreEqual("21,000", Argument.Create("${ price   =   21,000 }").Value);
+            Assert.AreEqual("price", Argument.Create("${price=$21,000}").Key);
+            Assert.AreEqual("$21,000", Argument.Create("${price=$21,000}").Value);
+        }
+
+        [TestMethod]
+        public void key_value_argument_place_in_curly_brackets_with_colon () {
+            Assert.AreEqual("price", Argument.Create("${price:21,000}").Key);
+            Assert.AreEqual("21,000", Argument.Create("${price:21,000}").Value);
+            Assert.AreEqual("price", Argument.Create("${price:  21,000     }").Key);
+            Assert.AreEqual("21,000", Argument.Create("${price:  21,000     }").Value);
+            Assert.AreEqual("price", Argument.Create("${    price :21,000}").Key);
+            Assert.AreEqual("21,000", Argument.Create("${    price :21,000}").Value);
+            Assert.AreEqual("price", Argument.Create("${ price   :   21,000 }").Key);
+            Assert.AreEqual("21,000", Argument.Create("${ price   :   21,000 }").Value);
+            Assert.AreEqual("price", Argument.Create("${price:$21,000}").Key);
+            Assert.AreEqual("$21,000", Argument.Create("${price:$21,000}").Value);
+        }
+
+    }
+
+}
