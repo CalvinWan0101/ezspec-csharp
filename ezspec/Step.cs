c0c87df8c7d52a1f79ef892e9d36be63cde180b2
CalvinWan0101 <calvinwan0101@gmail.com>
2023-11-19 13:33:23 +0800

    [Feature Addition] Added the constructor and Arguments for Step

---
+++ ezspec/Step.cs
@@ -0,0 +1,60 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ezSpec {
+    public class Step {
+
+        public delegate void StepCallback();
+
+        static public bool ContinuousAfterFailure {
+            get { return true; }
+        }
+
+        static public bool TerminateAfterFailure {
+            get { return false; }
+        }
+
+        private string description;
+        private StepCallback callback;
+        private bool continousAfterFailure;
+
+        public string Description { 
+            get { return description; }
+        }
+
+        public StepCallback Callback { 
+            get { return callback; }
+        }
+
+        public bool IsContinuousAfterFailure {
+            get { return continousAfterFailure; }
+        }
+
+        public List<Argument> Arguments {
+            get {
+                List<Argument> arguments = new List<Argument>();
+
+                Regex reg = new Regex("\\s\\$\\{([^}]+)\\}|\\s\\$([\\S^{]+)");
+                MatchCollection matchs = reg.Matches(description);
+                GroupCollection groups = matchs[0].Groups;
+
+                foreach(Match match in matchs) {
+                    arguments.Add(Argument.Create(match.Groups[0].Value));
+                }
+
+                return arguments;
+            }
+        }
+
+        public Step(string description, StepCallback callback) { 
+            this.description = description;
+            this.continousAfterFailure = false;
+            this.callback = callback;
+        }
+
+        public Step(string description, bool continous, StepCallback callback) {
+            this.description = description;
+            this.continousAfterFailure = continous;
+            this.callback= callback;
+        }
+    }
+}
---
+++ ezspec_test/StepTest.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ezSpec.Test {
+
+    [TestClass]
+    public class StepTest {
+
+        [TestMethod]
+        public void create_step() {
+            Step step = new Step("description", () => {});
+            Assert.AreEqual("description", step.Description);
+            Assert.IsFalse(step.IsContinuousAfterFailure);
+        }
+
+        [TestMethod]
+        public void create_step_with_continous() {
+            Step step = new Step("description", Step.ContinuousAfterFailure, () => {});
+            Assert.AreEqual("description", step.Description);
+            Assert.IsTrue(step.IsContinuousAfterFailure);
+        }
+
+        [TestMethod]
+        public void invoke_step_callback() {
+            int notifyCount = 0;
+            Step step = new Step("description", () => {
+                notifyCount++;
+            });
+
+            step.Callback();
+
+            Assert.AreEqual(1, notifyCount);
+        }
+
+        [TestMethod]
+        public void description_with_dollar_sign_argument() {
+            Step step = new Step("With argument $123", () => {});
+
+            List<Argument> arguments = step.Arguments;
+
+            Assert.AreEqual(1, arguments.Count);
+            Assert.AreEqual("", arguments[0].Key);
+            Assert.AreEqual("123", arguments[0].Value);
+        }
+
+        [TestMethod]
+        public void description_with_equal_sign_argument_in_curly_brackets() {
+            Step step = new Step("With argument ${arg=123}", () => {});
+
+            List<Argument> arguments = step.Arguments;
+
+            Assert.AreEqual(1, arguments.Count);
+            Assert.AreEqual("arg", arguments[0].Key);
+            Assert.AreEqual("123", arguments[0].Value);
+        }
+
+        [TestMethod]
+        public void description_with_colon_argument_in_curly_brackets() {
+            Step step = new Step("With argument ${arg:123}", () => { });
+
+            List<Argument> arguments = step.Arguments;
+
+            Assert.AreEqual(1, arguments.Count);
+            Assert.AreEqual("arg", arguments[0].Key);
+            Assert.AreEqual("123", arguments[0].Value);
+        }
+
+        [TestMethod]
+        public void description_with_many_arguments() {
+            Step step = new Step("With arrguments ${arg1:abc}, $$210 , and ${arg2=123}", () => {});
+
+            List<Argument> arguments = step.Arguments;
+
+            Assert.AreEqual(3, arguments.Count);
+            Assert.AreEqual("arg1", arguments[0].Key);
+            Assert.AreEqual("abc", arguments[0].Value);
+            Assert.AreEqual("", arguments[1].Key);
+            Assert.AreEqual("$210", arguments[1].Value);
+            Assert.AreEqual("arg2", arguments[2].Key);
+            Assert.AreEqual("123", arguments[2].Value);
+        }
+    }
+}
