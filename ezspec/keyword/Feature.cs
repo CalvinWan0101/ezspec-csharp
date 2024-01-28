c13a671a3f520d1b4cec22685276506449adb179
CalvinWan0101 <calvinwan0101@gmail.com>
2024-01-28 15:04:04 +0800

    [Feature Addition] Completed feature

--- ezspec/keyword/Feature.cs
+++ ezspec/keyword/Feature.cs
@@ -1,9 +1,13 @@
-﻿namespace ezSpec.keyword {
+﻿using System.Text;
+
+namespace ezSpec.keyword {
 
     public class Feature {
 
         private string name;
         private string description;
+        private IList<Rule> rules;
+        private Rule defaultRule;
 
         public string Name {
             get { return name; }
@@ -13,19 +17,15 @@
             get { return description; }
         }
 
-        public string FeatureText {
-            get {
-                string featureText = "Feature: " + Name;
-                if ("" != Description) {
-                    featureText += "\n\n" + Description;
-                }
-                return featureText;
-            }
+        public IList<Rule> Rules {
+            get { return rules; }
         }
 
         private Feature(string name, string description) {
             this.name = name;
             this.description = description;
+            this.defaultRule = Rule.New("", "");
+            this.rules = new List<Rule>();
         }
 
         public static Feature New(string name) {
@@ -36,8 +36,48 @@
             return new Feature(name, description);
         }
 
+        public void Initialize() {
+            defaultRule = Rule.New("", "");
+            rules.Clear();
+        }
+
+        public void NewRule(string name) {
+            NewRule(name, "");
+        }
+
+        public void NewRule(string name, string description) {
+            if (rules.Any(rule => rule.Name == name)) {
+                throw new ArgumentException("Rule name cannot be duplicated: " + name);
+            }
+            Rule rule = Rule.New(name, description);
+            rules.Add(rule);
+        }
+
+        public Rule WithDefaultRule() {
+            return defaultRule;
+        }
+
+        public Rule WithRule(string name) {
+            return rules.First(rule => rule.Name == name);
+        }
+
         public override string ToString() {
-            return FeatureText;
+            StringBuilder result = new StringBuilder();
+            result.Append("Feature: ");
+            result.Append(Name);
+            if ("" != Description) {
+                result.Append("\n\n")
+                    .Append(Description);
+            }
+            if (0 != defaultRule.Scenarios.Count) {
+                result.Append("\n\n");
+                result.Append(defaultRule.ToString());
+            }
+            foreach (Rule rule in rules) {
+                result.Append("\n\n");
+                result.Append(rule.ToString());
+            }
+            return result.ToString();
         }
     }
 }
--- ezspec_test/FeatureTest.cs
+++ ezspec_test/FeatureTest.cs
@@ -24,19 +24,218 @@ namespace ezSpec.keyword.Test {
         [TestMethod]
         public void get_feature_text() {
             Feature feature = Feature.New("name", "description");
-            Assert.AreEqual("Feature: name\n\ndescription", feature.FeatureText);
+            Assert.AreEqual("Feature: name\n\ndescription", feature.ToString());
         }
 
         [TestMethod]
         public void get_feature_text_without_description() {
             Feature feature = Feature.New("name");
-            Assert.AreEqual("Feature: name", feature.FeatureText);
+            Assert.AreEqual("Feature: name", feature.ToString());
         }
 
         [TestMethod]
         public void get_feature_string() {
             Feature feature = Feature.New("name", "description");
             Assert.AreEqual("Feature: name\n\ndescription", feature.ToString());
+        }
+
+        [TestMethod]
+        public void default_rule_created_when_new_feature() {
+            Feature feature = Feature.New("Feature's name", "Feature's description");
+
+            Assert.AreEqual("", feature.WithDefaultRule().Name);
+            Assert.AreEqual("", feature.WithDefaultRule().Description);
+        }
+
+        [TestMethod]
+        public void create_rule_with_name_and_description_by_feature() {
+            Feature feature = Feature.New("Feature's name", "Feature's description");
+
+            feature.NewRule("Rule's name", "Rule's description");
+
+            Assert.AreEqual("Rule's name", feature.WithRule("Rule's name").Name);
+            Assert.AreEqual("Rule's description", feature.WithRule("Rule's name").Description);
+        }
+
+        [TestMethod]
+        public void create_rule_with_name_by_feature() {
+            Feature feature = Feature.New("Feature's name", "Feature's description");
+
+            feature.NewRule("Rule's name");
+
+            Assert.AreEqual("Rule's name", feature.WithRule("Rule's name").Name);
+            Assert.AreEqual("", feature.WithRule("Rule's name").Description);
+        }
+
+        [TestMethod]
+        public void create_rule_by_feature_with_same_name() {
+            Feature feature = Feature.New("Feature's name", "Feature's description");
+
+            feature.NewRule("Rule's name");
+
+            Assert.ThrowsException<ArgumentException>(() => { feature.NewRule("Rule's name"); });
+        }
+
+        [TestMethod]
+        public void find_non_exist_rule() {
+            Feature feature = Feature.New("Feature's name", "Feature's description");
+
+            Assert.ThrowsException<InvalidOperationException>(() => feature.WithRule("rule non-existent"));
+        }
+
+        [TestMethod]
+        public void feature_initialization() {
+            Feature feature = Feature.New("Feature's name", "Feature's description");
+            feature.WithDefaultRule().NewScenario();
+            feature.NewRule("Rule's name");
+            feature.WithRule("Rule's name").NewScenario();
+
+            feature.Initialize();
+
+            Assert.AreEqual(0, feature.WithDefaultRule().Scenarios.Count);
+            Assert.AreEqual(0, feature.Rules.Count);
+        }
+
+        [TestMethod]
+        public void create_scenario_without_name() { 
+            Feature feature = Feature.New("Feature's name", "Feature's description");
+            RuntimeScenario scenario = feature.WithDefaultRule().NewScenario();
+
+            Assert.AreEqual("create scenario without name", scenario.Name);
+        }
+
+        [TestMethod]
+        public void get_feature_with_default_rule_with_scenario_string() {
+            Feature feature = Feature.New("Feature's name", "Feature's description");
+            feature.WithDefaultRule()
+                .NewScenario("Scenario's name")
+                .Given("given description", step.Step.ContinuousAfterFailure, env => { })
+                .When("when description", step.Step.ContinuousAfterFailure, env => { })
+                .Then("then description", step.Step.ContinuousAfterFailure, env => { });
+
+            string except =
+                "Feature: Feature's name\n" +
+                "\n" +
+                "Feature's description\n" +
+                "\n" +
+                "Scenario: Scenario's name\n" +
+                "[Pending] Given given description\n" +
+                "[Pending] When when description\n" +
+                "[Pending] Then then description";
+
+            Assert.AreEqual(except, feature.ToString());
+        }
+
+        [TestMethod]
+        public void get_feature_with_rule_with_scenario_string() {
+            Feature feature = Feature.New("Feature's name", "Feature's description");
+            feature.NewRule("Rule's name", "Rule's description");
+
+            feature.WithRule("Rule's name")
+                .NewScenario("Scenario's name")
+                .Given("given description", step.Step.ContinuousAfterFailure, env => { })
+                .When("when description", step.Step.ContinuousAfterFailure, env => { })
+                .Then("then description", step.Step.ContinuousAfterFailure, env => { });
+
+            string except =
+                "Feature: Feature's name\n" +
+                "\n" +
+                "Feature's description\n" +
+                "\n" +
+                "Rule: Rule's name\n" +
+                "Rule's description\n" +
+                "\n" +
+                "Scenario: Scenario's name\n" +
+                "[Pending] Given given description\n" +
+                "[Pending] When when description\n" +
+                "[Pending] Then then description";
+
+            Assert.AreEqual(except, feature.ToString());
+        }
+
+        [TestMethod]
+        public void get_feature_with_rule_without_scenario_string() {
+            Feature feature = Feature.New("Feature's name", "Feature's description");
+            feature.NewRule("Rule's name", "Rule's description");
+
+            string except =
+                "Feature: Feature's name\n" +
+                "\n" +
+                "Feature's description\n" +
+                "\n" +
+                "Rule: Rule's name\n" +
+                "Rule's description";
+
+            Assert.AreEqual(except, feature.ToString());
+        }
+
+        [TestMethod]
+        public void get_feature_with_multiple_rules_string() {
+            Feature feature = Feature.New("Feature's name", "Feature's description");
+            feature.NewRule("Rule1's name", "Rule1's description");
+            feature.NewRule("Rule2's name", "Rule2's description");
+
+            feature.WithRule("Rule2's name")
+                .NewScenario("Scenario's name")
+                .Given("given description", step.Step.ContinuousAfterFailure, env => { })
+                .When("when description", step.Step.ContinuousAfterFailure, env => { })
+                .Then("then description", step.Step.ContinuousAfterFailure, env => { });
+
+            string except =
+                "Feature: Feature's name\n" +
+                "\n" +
+                "Feature's description\n" +
+                "\n" +
+                "Rule: Rule1's name\n" +
+                "Rule1's description\n" +
+                "\n" +
+                "Rule: Rule2's name\n" +
+                "Rule2's description\n" +
+                "\n" +
+                "Scenario: Scenario's name\n" +
+                "[Pending] Given given description\n" +
+                "[Pending] When when description\n" +
+                "[Pending] Then then description";
+
+            Assert.AreEqual(except, feature.ToString());
+        }
+
+        [TestMethod]
+        public void get_feature_with_both_default_rule_and_rules_string() {
+            Feature feature = Feature.New("Feature's name", "Feature's description");
+            feature.NewRule("Rule's name", "Rule's description");
+
+            feature.WithDefaultRule()
+                .NewScenario("Scenario's name")
+                .Given("given description", step.Step.ContinuousAfterFailure, env => { })
+                .When("when description", step.Step.ContinuousAfterFailure, env => { })
+                .Then("then description", step.Step.ContinuousAfterFailure, env => { });
+           
+            feature.WithRule("Rule's name")
+                .NewScenario("Scenario's name")
+                .Given("given description", step.Step.ContinuousAfterFailure, env => { })
+                .When("when description", step.Step.ContinuousAfterFailure, env => { })
+                .Then("then description", step.Step.ContinuousAfterFailure, env => { });
+
+            string except =
+                "Feature: Feature's name\n" +
+                "\n" +
+                "Feature's description\n" +
+                "\n" +
+                "Scenario: Scenario's name\n" +
+                "[Pending] Given given description\n" +
+                "[Pending] When when description\n" +
+                "[Pending] Then then description\n" +
+                "\n" +
+                "Rule: Rule's name\n" +
+                "Rule's description\n" +
+                "\n" +
+                "Scenario: Scenario's name\n" +
+                "[Pending] Given given description\n" +
+                "[Pending] When when description\n" +
+                "[Pending] Then then description";
+
+            Assert.AreEqual(except, feature.ToString());
         }
     }
 
