41f65f691383eb7f400119cfa748b23e98c817a3
CalvinWan0101 <calvinwan0101@gmail.com>
2024-02-03 14:17:18 +0800

    [Feature Addition] Completed Examples

---
+++ ezspec/keyword/Examples.cs
@@ -0,0 +1,92 @@
+﻿using ezSpec.keyword.table;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ezSpec.keyword {
+    public class Examples {
+        private string name;
+        private string description;
+        private Table table;
+
+        public string Name {
+            get { return name; }
+        }
+
+        public string Description {
+            get { return description; }
+        }
+
+        public ReadOnlyCollection<Example> ExampleSet {
+            get {
+                // TODO: Use convertAll after updating Table
+                List<Example> examples = new List<Example>();
+                foreach (Row row in table.Rows) {
+                    examples.Add(Example.New(row));
+                }
+                return new ReadOnlyCollection<Example>(examples);
+            }
+        }
+
+        public static Examples New(string name, string description, Table table) {
+            return new Examples(name, description, table);
+        }
+
+        public static Examples New(string name, string description, string rawTable) {
+            return new Examples(name, description, Table.New(rawTable));
+        }
+
+        public static Examples New(string name, Table table) {
+            return new Examples(name, "", table);
+        }
+
+        public static Examples New(string name, string rawTable) {
+            return new Examples(name, "", Table.New(rawTable));
+        }
+
+        public static Examples New(Table table) {
+            return new Examples("", "", table);
+        }
+
+        public static Examples New(string rawTable) {
+            return new Examples("", "", Table.New(rawTable));
+        }
+
+        public static Examples New(Examples examples) {
+            return new Examples(examples.Name, examples.Description, examples.table);
+        }
+
+        private Examples(string name, string description, Table table) {
+            this.name = name;
+            this.description = description;
+            this.table = table;
+        }
+
+        public override string ToString() {
+            StringBuilder result = new StringBuilder();
+            result.Append("Examples: ");
+            result.Append(name);
+            result.Append("\n");
+            if ("" != description) {
+                result.Append(description);
+                result.Append("\n");
+            }
+            result.Append(table.ToString());
+
+            return result.ToString();
+        }
+
+        public string ToStringBeautify() {
+            StringBuilder result = new StringBuilder();
+            result.Append("Examples: ");
+            result.Append(name);
+            result.Append("\n");
+            if ("" != description) {
+                result.Append(description);
+                result.Append("\n");
+            }
+            result.Append(table.ToStringBeautify());
+
+            return result.ToString();
+        }
+    }
+}
---
+++ ezspec_test/ExamplesTest.cs
@@ -0,0 +1,220 @@
+﻿using ezSpec.keyword;
+using ezSpec.keyword.table;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ezSpec_test {
+
+    [TestClass]
+    public class ExamplesTest {
+
+        [TestMethod]
+        public void create_examples() {
+            Table table = Table.New(@"
+                |  id   |  name  | score |
+                | 10001 |  Joe   |  60   |
+                | 10002 | Calvin |  80   |
+                | 10003 | Howard |  100  |");
+
+            Examples examples = Examples.New("examplesName", "examplesDescription", table);
+
+            Assert.AreEqual("examplesName", examples.Name);
+            Assert.AreEqual("examplesDescription", examples.Description);
+            Assert.AreEqual(3, examples.ExampleSet.Count);
+            Assert.AreEqual("|\t10001\t|\tJoe\t|\t60\t|", examples.ExampleSet[0].ToString());
+            Assert.AreEqual("|\t10002\t|\tCalvin\t|\t80\t|", examples.ExampleSet[1].ToString());
+            Assert.AreEqual("|\t10003\t|\tHoward\t|\t100\t|", examples.ExampleSet[2].ToString());
+        }
+
+        [TestMethod]
+        public void create_examples_without_description() {
+            Table table = Table.New(@"
+                |  id   |  name  | score |
+                | 10001 |  Joe   |  60   |
+                | 10002 | Calvin |  80   |
+                | 10003 | Howard |  100  |");
+
+            Examples examples = Examples.New("examplesName", table);
+
+            Assert.AreEqual("examplesName", examples.Name);
+            Assert.AreEqual("", examples.Description);
+            Assert.AreEqual(3, examples.ExampleSet.Count);
+            Assert.AreEqual("|\t10001\t|\tJoe\t|\t60\t|", examples.ExampleSet[0].ToString());
+            Assert.AreEqual("|\t10002\t|\tCalvin\t|\t80\t|", examples.ExampleSet[1].ToString());
+            Assert.AreEqual("|\t10003\t|\tHoward\t|\t100\t|", examples.ExampleSet[2].ToString());
+        }
+
+        [TestMethod]
+        public void create_examples_with_only_table() {
+            Table table = Table.New(@"
+                |  id   |  name  | score |
+                | 10001 |  Joe   |  60   |
+                | 10002 | Calvin |  80   |
+                | 10003 | Howard |  100  |");
+
+            Examples examples = Examples.New(table);
+
+            Assert.AreEqual("", examples.Name);
+            Assert.AreEqual("", examples.Description);
+            Assert.AreEqual(3, examples.ExampleSet.Count);
+            Assert.AreEqual("|\t10001\t|\tJoe\t|\t60\t|", examples.ExampleSet[0].ToString());
+            Assert.AreEqual("|\t10002\t|\tCalvin\t|\t80\t|", examples.ExampleSet[1].ToString());
+            Assert.AreEqual("|\t10003\t|\tHoward\t|\t100\t|", examples.ExampleSet[2].ToString());
+        }
+
+        [TestMethod]
+        public void create_examples_with_raw_table() {
+            string rawData = @"
+                |  id   |  name  | score |
+                | 10001 |  Joe   |  60   |
+                | 10002 | Calvin |  80   |
+                | 10003 | Howard |  100  |";
+
+            Examples examples = Examples.New("examplesName", "examplesDescription", rawData);
+
+            Assert.AreEqual("examplesName", examples.Name);
+            Assert.AreEqual("examplesDescription", examples.Description);
+            Assert.AreEqual(3, examples.ExampleSet.Count);
+            Assert.AreEqual("|\t10001\t|\tJoe\t|\t60\t|", examples.ExampleSet[0].ToString());
+            Assert.AreEqual("|\t10002\t|\tCalvin\t|\t80\t|", examples.ExampleSet[1].ToString());
+            Assert.AreEqual("|\t10003\t|\tHoward\t|\t100\t|", examples.ExampleSet[2].ToString());
+        }
+
+        [TestMethod]
+        public void create_examples_with_raw_table_without_description() {
+            string rawData = @"
+                |  id   |  name  | score |
+                | 10001 |  Joe   |  60   |
+                | 10002 | Calvin |  80   |
+                | 10003 | Howard |  100  |";
+
+            Examples examples = Examples.New("examplesName", rawData);
+
+            Assert.AreEqual("examplesName", examples.Name);
+            Assert.AreEqual("", examples.Description);
+            Assert.AreEqual(3, examples.ExampleSet.Count);
+            Assert.AreEqual("|\t10001\t|\tJoe\t|\t60\t|", examples.ExampleSet[0].ToString());
+            Assert.AreEqual("|\t10002\t|\tCalvin\t|\t80\t|", examples.ExampleSet[1].ToString());
+            Assert.AreEqual("|\t10003\t|\tHoward\t|\t100\t|", examples.ExampleSet[2].ToString());
+        }
+
+        [TestMethod]
+        public void create_examples_with_only_raw_table() {
+            string rawData = @"
+                |  id   |  name  | score |
+                | 10001 |  Joe   |  60   |
+                | 10002 | Calvin |  80   |
+                | 10003 | Howard |  100  |";
+
+            Examples examples = Examples.New(rawData);
+
+            Assert.AreEqual("", examples.Name);
+            Assert.AreEqual("", examples.Description);
+            Assert.AreEqual(3, examples.ExampleSet.Count);
+            Assert.AreEqual("|\t10001\t|\tJoe\t|\t60\t|", examples.ExampleSet[0].ToString());
+            Assert.AreEqual("|\t10002\t|\tCalvin\t|\t80\t|", examples.ExampleSet[1].ToString());
+            Assert.AreEqual("|\t10003\t|\tHoward\t|\t100\t|", examples.ExampleSet[2].ToString());
+        }
+
+        [TestMethod]
+        public void create_examples_with_examples() {
+            string rawData = @"
+                |  id   |  name  | score |
+                | 10001 |  Joe   |  60   |
+                | 10002 | Calvin |  80   |
+                | 10003 | Howard |  100  |";
+
+            Examples examples = Examples.New("examplesName", "examplesDescription", rawData);
+            Examples examplesCopy = Examples.New(examples);
+
+            Assert.AreEqual("examplesName", examplesCopy.Name);
+            Assert.AreEqual("examplesDescription", examplesCopy.Description);
+            Assert.AreEqual(3, examplesCopy.ExampleSet.Count);
+            Assert.AreEqual("|\t10001\t|\tJoe\t|\t60\t|", examplesCopy.ExampleSet[0].ToString());
+            Assert.AreEqual("|\t10002\t|\tCalvin\t|\t80\t|", examplesCopy.ExampleSet[1].ToString());
+            Assert.AreEqual("|\t10003\t|\tHoward\t|\t100\t|", examplesCopy.ExampleSet[2].ToString());
+        }
+
+        [TestMethod]
+        public void examples_to_string() {
+            string rawData = @"
+                |  id   |  name  | score |
+                | 10001 |  Joe   |  60   |
+                | 10002 | Calvin |  80   |
+                | 10003 | Howard |  100  |";
+
+            Examples examples = Examples.New("examplesName", "examplesDescription", rawData);
+
+            string expect =
+                "Examples: examplesName\n" +
+                "examplesDescription\n" +
+                "|\tid\t|\tname\t|\tscore\t|\n" +
+                "|\t10001\t|\tJoe\t|\t60\t|\n" +
+                "|\t10002\t|\tCalvin\t|\t80\t|\n" +
+                "|\t10003\t|\tHoward\t|\t100\t|";
+
+            Assert.AreEqual(expect, examples.ToString());
+        }
+
+        [TestMethod]
+        public void examples_to_string_without_description() {
+            string rawData = @"
+                |  id   |  name  | score |
+                | 10001 |  Joe   |  60   |
+                | 10002 | Calvin |  80   |
+                | 10003 | Howard |  100  |";
+
+            Examples examples = Examples.New("examplesName", "", rawData);
+
+            string expect =
+                "Examples: examplesName\n" +
+                "|\tid\t|\tname\t|\tscore\t|\n" +
+                "|\t10001\t|\tJoe\t|\t60\t|\n" +
+                "|\t10002\t|\tCalvin\t|\t80\t|\n" +
+                "|\t10003\t|\tHoward\t|\t100\t|";
+
+            Assert.AreEqual(expect, examples.ToString());
+        }
+
+        [TestMethod]
+        public void examples_to_string_with_only_raw_table() {
+            string rawData = @"
+                |  id   |  name  | score |
+                | 10001 |  Joe   |  60   |
+                | 10002 | Calvin |  80   |
+                | 10003 | Howard |  100  |";
+
+            Examples examples = Examples.New("", "", rawData);
+
+            string expect =
+                "Examples: \n" +
+                "|\tid\t|\tname\t|\tscore\t|\n" +
+                "|\t10001\t|\tJoe\t|\t60\t|\n" +
+                "|\t10002\t|\tCalvin\t|\t80\t|\n" +
+                "|\t10003\t|\tHoward\t|\t100\t|";
+
+            Assert.AreEqual(expect, examples.ToString());
+        }
+
+        [TestMethod]
+        public void examples_to_string_beautify() {
+            string rawData = @"
+                |  id   |  name  | score |
+                | 10001 |  Joe   |  60   |
+                | 10002 | Calvin |  80   |
+                | 10003 | Howard |  100  |";
+
+            Examples examples = Examples.New("examplesName", "examplesDescription", rawData);
+
+
+            string expect =
+                "Examples: examplesName\n" +
+                "examplesDescription\n" +
+                "| id    | name   | score |\n" +
+                "| 10001 | Joe    | 60    |\n" +
+                "| 10002 | Calvin | 80    |\n" +
+                "| 10003 | Howard | 100   |";
+
+            Assert.AreEqual(expect, examples.ToStringBeautify());
+        }
+    }
+}
