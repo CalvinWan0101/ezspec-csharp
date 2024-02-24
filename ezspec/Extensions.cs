3a101240cc770139393281c2094cc4c614438396
Zch720 <92720howard@gmail.com>
2024-02-24 13:46:14 +0800

    [Bug Fixing] Fixed AutoTrim can not work with different breakline

--- ezspec/Extensions.cs
+++ ezspec/Extensions.cs
@@ -1,12 +1,7 @@
 ﻿public static class Extensions {
     public static string AutoTrim(this string code) {
-        string newline = Environment.NewLine;
-        var trimLen = code
-            .Split(newline)
-            .Skip(1)
-            .Min(s => s.Length - s.TrimStart().Length);
-
-        List<string> lines = code.Split(newline).ToList();
+        List<string> lines = SplitByNewLine(code);
+        int trimLen = lines.Skip(1).Min(s => s.Length - s.TrimStart().Length);
 
         if (lines.Count == 0) {
             return "";
@@ -18,5 +13,17 @@
             lines
             .GetRange(1, lines.Count - 1)
             .ConvertAll(line => line.Substring(Math.Min(line.Length, trimLen))));
+    }
+
+    private static List<string> SplitByNewLine(string code) {
+        if (code.Contains("\r\n")) {
+            return code.Split("\r\n").ToList();
+        }
+        else if (code.Contains("\r")) {
+            return code.Split("\r").ToList();
+        }
+        else {
+            return code.Split("\n").ToList();
+        }
     }
 }
